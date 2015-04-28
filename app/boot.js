ba2934b4e6162fbb50cef65cfb6f06e0c52e9fb3
entymon <pawel.olejniczak.i@gmail.com>
2015-04-28 22:05:38 +0100

    test

--- app/boot.js
+++ app/boot.js
@@ -1,58 +1,24 @@
-var backoffice = angular.module('Backoffice', [
-		'ngResource',
+var application = angular.module('BackofficeApp', [
 		'ui.router',
-		'angularFileUpload',
-		'ngAnimate',
-		'ngAria',
-		'ngMaterial',
-		'entymonFilters',
-		'entymonDirectives'
 	]);
 
-backoffice
-  	.config(['$stateProvider', '$urlRouterProvider', '$resourceProvider', '$httpProvider','$mdThemingProvider',
-  		function ($stateProvider, $urlRouterProvider, $resourceProvider, $httpProvider,$mdThemingProvider) {
+application
+	.controller('DashboardCtrl',['$log','$scope','$state',
+		function ($log,$scope,$state) {
+
+			$scope.demo = "About me View";
+
+		}])
+
+
+  	.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+  		function ($stateProvider, $urlRouterProvider, $httpProvider) {
 	        $stateProvider
 
-	        	// home
 	        	.state('dashboard', {
-					url: '/',
+					url: '/dashboard',
 					controller: 'DashboardCtrl',
-					data: {
-						roles: []
-					},
-					templateUrl: 'app/controllers/dashboard.html.tmpl'
-				})
+					templateUrl: 'app/controllers/dashboard.html.tmpl' 
+				});
 
-	        	// login 
-	        	.state('login', {
-					url: '/login',
-					controller: 'LoginCtrl',
-					data: {
-						roles: []
-					},
-					templateUrl: 'app/controllers/login.html.tmpl'
-				})
-			;
-
-			$urlRouterProvider.otherwise('/');
-
-			$mdThemingProvider
-				.theme('docs-dark', 'default')
-		        .primaryPalette('blue');
-
-	}])
-	.run(['$rootScope', '$state', '$stateParams','authorization','principal',
-    	function($rootScope, $state, $stateParams,authorization,principal) {
-      		$rootScope.$on('$stateChangeStart', function(event, toState, toStateParams) {
-	        	// track the state the user wants to go to; authorization service needs this
-	        	$rootScope.toState = toState;
-	        	$rootScope.toStateParams = toStateParams;
-		        // if the principal is resolved, do an authorization check immediately. otherwise,
-		        // it'll be done when the state it resolved.
-		        if (principal.isIdentityResolved()) {
-		        	authorization.authorize();
-		        }
-	      	});
-    	}
-  	]);
+	}]);
