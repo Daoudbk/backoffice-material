027011e767e05495c1b6e79cea9eabb8ab1fe693
pawel_olejniczak <pawel.olejniczak@gimo.co.uk>
2015-04-29 14:26:05 +0100

    introduction to new backend

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,7 +1,37 @@
 application
-	.controller('DashboardCtrl',['$log','$scope','$state',
-		function ($log,$scope,$state) {
+	.controller('DashboardCtrl',['$log','$scope','$state','$timeout','$mdSidenav','$mdUtil',
+		function ($log,$scope,$state,$timeout,$mdSidenav,$mdUtil) {
 
-			$scope.demo = "DashboardCtrl";
-
-		}]);
+			$scope.toggleLeft = buildToggler('left');
+		    $scope.toggleRight = buildToggler('right');
+		    /**
+		     * Build handler to open/close a SideNav; when animation finishes
+		     * report completion in console
+		     */
+		    function buildToggler(navID) {
+		      var debounceFn =  $mdUtil.debounce(function(){
+		            $mdSidenav(navID)
+		              .toggle()
+		              .then(function () {
+		                $log.debug("toggle " + navID + " is done");
+		              });
+		          },300);
+		      return debounceFn;
+		    };
+	}])
+	.controller('LeftCtrl', 
+		function ($scope, $timeout, $mdSidenav, $log) {
+    		$scope.close = function () {
+      			$mdSidenav('left').close().then(function () {
+          		$log.debug("close LEFT is done");
+        	});
+    	};
+  	})
+  	.controller('RightCtrl', 
+  		function ($scope, $timeout, $mdSidenav, $log) {
+   	 		$scope.close = function () {
+      			$mdSidenav('right').close().then(function () {
+          		$log.debug("close RIGHT is done");
+        	});
+    	};
+  	});
