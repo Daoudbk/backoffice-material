e35ddb9edd5fd0e1634d83379452d34a5d69e3d7
entymon <pawel.olejniczak.i@gmail.com>
2015-05-01 00:09:36 +0100

    pagination introduction

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -55,30 +55,7 @@ application
       			$mdSidenav('right').close().then(function () {
         	});
     	};
-  	})
+  	});
 
 
-      .controller('AppCtrl', function ($scope, $log) {
-    var tabs = [
-          { title: 1, content: "Tabs will become paginated if there isn't enough room for them."},
-          { title: 2, content: "You can swipe left and right on a mobile device to change tabs."},
-          { title: 3, content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
-          { title: 4, content: "If you set the selected tab binding to -1, it will leave no tab selected."},
-          { title: 5, content: "If you remove a tab, it will try to select a new one."},
-          { title: 6, content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
-          { title: 7, content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
-          { title: 8, content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
-          { title: 9, content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
-          { title: 10, content: "If you're still reading this, you should just go check out the API docs for tabs!"}
-        ],
-        selected = null,
-        previous = null;
-    $scope.tabs = tabs;
-    $scope.selectedIndex = 2;
-    $scope.$watch('selectedIndex', function(current, old){
-      // previous = selected;
-      // selected = tabs[current];
-      // if ( old && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
-      // if ( current )                $log.debug('Hello ' + selected.title + '!');
-    });
-  });
+ 
--- app/controllers/post/list.js
+++ app/controllers/post/list.js
@@ -2,6 +2,7 @@ application
 	.controller('PostListCtrl',['$log','$scope','$state','$timeout','$mdSidenav','$mdUtil',
 		function ($log,$scope,$state,$timeout,$mdSidenav,$mdUtil) {
 
+			$scope.elementsPerPage = 5;
 		    $scope.posts = [
 		    	{
 		    		id: 1,
@@ -43,6 +44,51 @@ application
 		    		author: 'user',
 		    		created: '12:12:12 13-04-2015',
 		    	},
+		    	{
+		    		id: 6,
+		    		title: 'This is title',
+		    		category: 'Babilnonian',
+		    		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		    		author: 'user',
+		    		created: '12:12:12 13-04-2015',
+		    	},
+		    	{
+		    		id: 7,
+		    		title: 'This is title',
+		    		category: 'Babilnonian',
+		    		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		    		author: 'user',
+		    		created: '12:12:12 13-04-2015',
+		    	},
 		    ];
+
+		$scope.totalElements = $scope.posts.length;
+
+		// Always round number to up
+		var pages = Math.ceil($scope.totalElements / $scope.elementsPerPage);
+
+		$log.debug('pages');
+		$log.debug(pages);
+
+		var tabs = [
+			{ title: 1, content: "Tabs will become paginated if there isn't enough room for them."},
+			{ title: 2, content: "You can swipe left and right on a mobile device to change tabs."},
+			{ title: 3, content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
+			{ title: 4, content: "If you set the selected tab binding to -1, it will leave no tab selected."},
+			{ title: 5, content: "If you remove a tab, it will try to select a new one."},
+			{ title: 6, content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
+			{ title: 7, content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
+			{ title: 8, content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
+			{ title: 9, content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
+			{ title: 10, content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+        ],
+        selected = null,
+        previous = null;
+
+	    $scope.tabs = tabs;
+	    $scope.selectedIndex = 2;
+	    $scope.$watch('selectedIndex', function(current, old){
+	   
+		});
 
 	}]);
