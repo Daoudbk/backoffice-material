4f8adb7a9b42dafcd958a8cbb42409d36d6fd43f
entymon <pawel.olejniczak.i@gmail.com>
2015-05-04 13:25:07 +0100

    continue with view gallery template added

--- app/controllers/post/create.js
+++ app/controllers/post/create.js
@@ -23,4 +23,50 @@ application
 				},
 			];
 		}
+	])
+	.controller('CreatePostGalleryCtrl',['$log','$scope','$state','$timeout','$mdSidenav','$mdUtil',
+		function ($log,$scope,$state,$timeout,$mdSidenav,$mdUtil) {
+
+			var galleries = [
+				{
+					id: 1,
+					title: "Example Image 1",
+					path: "/path/to/image",
+					status: false
+				},
+				{
+					id: 2,
+					title: "Example Image 2",
+					path: "/path/to/image",
+					status: false
+				},
+				{
+					id: 3,
+					title: "Example Image 3",
+					path: "/path/to/image",
+					status: false
+				},
+				{
+					id: 4,
+					title: "Example Image 4",
+					path: "/path/to/image",
+					status: false
+				},
+				{
+					id: 5,
+					title: "Example Image 5",
+					path: "/path/to/image",
+					status: false
+				},
+				{
+					id: 6,
+					title: "Example Image 6",
+					path: "/path/to/image",
+					status: false
+				}
+			];
+
+			
+
+		}
 	]);
