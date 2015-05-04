e87dfed576770729a0322f8adcb01b93fb9bbb2f
entymon <pawel.olejniczak.i@gmail.com>
2015-05-04 13:50:55 +0100

    update view for create about gallery

--- app/controllers/post/create.js
+++ app/controllers/post/create.js
@@ -66,6 +66,15 @@ application
 				}
 			];
 
+			$scope.data = {
+			    cb1: true,
+			    cb4: true,
+			    cb5: false
+			};
+			$scope.onChange = function(cbState) {
+				$scope.message = "The switch is now: " + cbState;
+			};
+
 			
 
 		}
