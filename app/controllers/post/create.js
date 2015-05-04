f002fafd25c430229cd19f7703dfaadc18bd9ac0
entymon <pawel.olejniczak.i@gmail.com>
2015-05-04 12:38:03 +0100

    add main form for create and edit post

--- app/controllers/post/create.js
+++ app/controllers/post/create.js
@@ -2,5 +2,25 @@ application
 	.controller('PostCreateCtrl',['$log','$scope','$state','$timeout','$mdSidenav','$mdUtil',
 		function ($log,$scope,$state,$timeout,$mdSidenav,$mdUtil) {
 
+			$scope.post = {
+				title: '',
+				header: '',
+				content: '',
+			}
+
+			$scope.categories = [
+				{
+					id: 1,
+					name: 'category 1'
+				},
+				{
+					id: 2,
+					name: 'category 2'
+				},
+				{
+					id: 3,
+					name: 'category 3'
+				},
+			];
 		}
 	]);
